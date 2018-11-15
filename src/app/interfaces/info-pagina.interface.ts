5fc0358b0e3d3c20aee539e8fdae8d3369c05ff0
IGNACIO MONDRAGON <immondrag@hotmail.com>
2018-11-15 17:00:55 -0600

    Fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,12 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nomre_corto?: string;
+  paginadel_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
